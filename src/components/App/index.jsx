107e7ced3157bff85fed9783902f74b4502420ee
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-11-15 20:56:07 -0300

    Initial commit

---
+++ src/components/App/index.jsx
@@ -0,0 +1,25 @@
+import ListOfCards from '../ListOfCards'
+import './App.css'
+
+function App () {
+  const randomCardList = [
+    { text: 'Card 1', image: 'https://unsplash.it/id/201/420/420'},
+    { text: 'Card 2', image: 'https://unsplash.it/id/202/420/420'},
+    { text: 'Card 3', image: 'https://unsplash.it/id/203/420/420'},
+    { text: 'Card 4', image: 'https://unsplash.it/id/204/420/420'},
+    { text: 'Card 5', image: 'https://unsplash.it/id/205/420/420'},
+    { text: 'Card 6', image: 'https://unsplash.it/id/206/420/420'},
+    { text: 'Card 7', image: 'https://unsplash.it/id/207/420/420'},
+    { text: 'Card 8', image: 'https://unsplash.it/id/208/420/420'},
+    { text: 'Card 9', image: 'https://unsplash.it/id/209/420/420'},
+  ]
+
+  return (
+    <main>
+      <h1>Renderizado de listas</h1>
+      <ListOfCards list={randomCardList} />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Card/index.jsx
@@ -0,0 +1,9 @@
+import './Card.css'
+
+function Card (props) {
+  return (
+    <article></article>
+  )
+}
+
+export default Card
